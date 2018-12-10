dfcc6c023a49787552748be3577b1958c1beb7bd
CGDF <17256673+CGDF-Github@users.noreply.github.com>
2018-12-10 11:16:01 +0800

    update to 0.1.8

--- shadowsocks-csharp/_SSD/Controller/Service/UpdateChecker/UpdateChecker.cs
+++ shadowsocks-csharp/_SSD/Controller/Service/UpdateChecker/UpdateChecker.cs
@@ -2,7 +2,7 @@
     public partial class UpdateChecker {
         private const string UPDATE_URL_SSD = "https://api.github.com/repos/CGDF-GitHub/SSD-Windows/releases";
         public const string RELEASE_URL_SSD = "https://github.com/CGDF-Github/SSD-Windows/releases";
-        private const string VERSION_BASIC = "4.1.3";
+        private const string VERSION_BASIC = "4.1.3.1";
         private const string VERSION_SSD = "0.1.8";
     }
 }
