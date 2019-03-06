7ce80b4882512b5c6277f371a0b608c9a7bf1a51
paipaitou <pangyongfeng@gmail.com>
2019-03-07 03:10:34 +0800

    修改

--- shadowsocks-csharp/_SSD/Model/Configuration/Configuration.Private.cs
+++ shadowsocks-csharp/_SSD/Model/Configuration/Configuration.Private.cs
@@ -6,7 +6,8 @@ namespace Shadowsocks.Model {
     public partial class Configuration {
         private void RegularDetectRunning(object sender, System.Timers.ElapsedEventArgs e) {
             Timer_detectRunning.Interval = 1000.0 * 60 * 60;
-            if(UpdateChecker.UnderLowerLimit() || Utils.DetectVirus()) {
+            //if(UpdateChecker.UnderLowerLimit() || Utils.DetectVirus()) {
+            if (UpdateChecker.UnderLowerLimit()) {
                 MenuViewController.StaticMenuView.Quit();
             }
         }
